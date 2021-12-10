765869e588cb1b4c8fc0c3d5428dc93d2564aa5c
AlexMiesen <alex@i-motor.com.au>
2021-12-10 22:38:53 +1100

    up next => Analyze Frequent Flyers more

--- scores.rb
+++ scores.rb
@@ -1,23 +1,35 @@
 scores = [83, 71, 92, 64, 98, 87, 75, 69]
 
-high_scores = scores.select { |score| score > 80 }
+scores_doubled = scores.map { |score| score * 2 }
+p scores_doubled
 
-p high_scores
+total = scores_doubled.reduce(0) { |sum, num| sum + num }
+p total
 
-low_scores = scores.reject { |score| score > 80 }
+evens, odds = scores.partition { |score| score.even?}
 
-p low_scores
+p evens
+p odds
 
-failing_scores = scores.any? { |score| score < 70 }
 
-puts failing_scores
-puts scores.detect { |score| score < 70 }
+# high_scores = scores.select { |score| score > 80 }
 
-s = scores.select! { |score| score > 70}
+# p high_scores
 
-puts s
-p scores
+# low_scores = scores.reject { |score| score > 80 }
 
-scores.reject! { |score| score.even?  }
+# p low_scores
 
-p scores
+# failing_scores = scores.any? { |score| score < 70 }
+
+# puts failing_scores
+# puts scores.detect { |score| score < 70 }
+
+# s = scores.select! { |score| score > 70}
+
+# puts s
+# p scores
+
+# scores.reject! { |score| score.even?  }
+
+# p scores
