9e6bcdb7db258bcb8cb921216d02bafae3f54b11
AlexMiesen <alex@i-motor.com.au>
2022-03-19 17:28:54 +1100

    included Enumerable try out it out in the Playlist class

--- songs.rb
+++ songs.rb
@@ -17,6 +17,7 @@ song_2 = Song.new('Song 2','Blur', 2)
 song_3 = Song.new('The Magic Number','De La Soul', 3)
 
 class Playlist
+	include Enumerable
   def initialize(name)
     @name = name
     @songs = []
@@ -27,7 +28,10 @@ class Playlist
   end
 
 	def each
-		@songs.each { |song| yield song }
+		@songs.each do |song| 
+			puts "Yielding #{song.name}..." 
+			yield song
+		end
 	end
 
 	def play_songs
@@ -44,3 +48,16 @@ my_playlist.add_song(song_3)
 # my_playlist.each {|song| song.play}
 
 my_playlist.play_songs
+
+g_songs = my_playlist.select { |song| song.name =~ /g/ } #only songs with the letter 'G'
+
+john_songs = my_playlist.select { |song| song.artist =~ /John/}
+
+total_duration = my_playlist.sum { |song| song.duration }
+
+p g_songs
+p john_songs
+p total_duration
+
+song_labels = my_playlist.map { |song| "#{song.name} - #{song.artist}" }
+p song_labels
