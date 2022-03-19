28a676357edea6507e1fa4e91a093662a1633978
AlexMiesen <alex@i-motor.com.au>
2022-03-19 19:11:14 +1100

    WIP - each_filename method

--- songs.rb
+++ songs.rb
@@ -10,11 +10,15 @@ class Song
   def play
     puts "Playing '#{name}' by #{artist} (#{duration} mins)..."
   end
+
+	def each_filename
+		self.each {|song| song.gsub(" ", "-")}
+	end
 end
 
-song_1 = Song.new('Wild Blue','John Mayer', 3)
-song_2 = Song.new('Song 2','Blur', 2)
-song_3 = Song.new('The Magic Number','De La Soul', 3)
+song1 = Song.new('Wild Blue','John Mayer', 3)
+song2 = Song.new('Song 2','Blur', 2)
+song3 = Song.new('The Magic Number','De La Soul', 3)
 
 class Playlist
 	include Enumerable
@@ -37,13 +41,22 @@ class Playlist
 	def play_songs
 		self.each { |song| song.play }
 	end
+
+	def each_tagline
+		@songs.each {|song| yield "#{song.name} - #{song.artist}" }
+	end
+
+	def each_by_artist(artist)
+		@songs.select {|song| song.artist == artist }.each {|song| yield song}
+	end
+
 end
 
 my_playlist = Playlist.new('epic list')
 
-my_playlist.add_song(song_1)
-my_playlist.add_song(song_2)
-my_playlist.add_song(song_3)
+my_playlist.add_song(song1)
+my_playlist.add_song(song2)
+my_playlist.add_song(song3)
 
 # my_playlist.each {|song| song.play}
 
@@ -55,9 +68,15 @@ john_songs = my_playlist.select { |song| song.artist =~ /John/}
 
 total_duration = my_playlist.sum { |song| song.duration }
 
-p g_songs
-p john_songs
-p total_duration
+# p g_songs
+# p john_songs
+# p total_duration
 
 song_labels = my_playlist.map { |song| "#{song.name} - #{song.artist}" }
-p song_labels
+# p song_labels
+
+my_playlist.each_tagline { |tagline| puts tagline}
+my_playlist.each_by_artist("Blur") { |song| song.play}
+
+song1.each_filename {|filename| puts filename}
+
