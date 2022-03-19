59232c52107c333c8d6168dc41ec7af14353ee2b
AlexMiesen <alex@i-motor.com.au>
2022-03-19 16:17:32 +1100

    added a my_playlist that yields each song

--- songs.rb
+++ songs.rb
@@ -16,10 +16,6 @@ song_1 = Song.new('Wild Blue','John Mayer', 3)
 song_2 = Song.new('Song 2','Blur', 2)
 song_3 = Song.new('The Magic Number','De La Soul', 3)
 
-song_1.play
-song_2.play
-song_3.play
-
 class Playlist
   def initialize(name)
     @name = name
@@ -29,6 +25,10 @@ class Playlist
   def add_song(song)
     @songs << song
   end
+
+	def each
+		@songs.each { |song| yield song }
+	end
 end
 
 my_playlist = Playlist.new('epic list')
@@ -36,3 +36,5 @@ my_playlist = Playlist.new('epic list')
 my_playlist.add_song(song_1)
 my_playlist.add_song(song_2)
 my_playlist.add_song(song_3)
+
+my_playlist.each {|song| song.play}
