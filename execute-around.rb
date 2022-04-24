9bb44922d8eb518a40aa188b45d273a816fc89eb
AlexMiesen <alex@i-motor.com.au>
2022-04-24 16:30:38 +1000

    Added with_debugging method

--- execute-around.rb
+++ execute-around.rb
@@ -28,3 +28,18 @@ tag(:ul) do
   tag(:li) { "It shines!"}
   tag(:li) { "It mesmerizes!"}
 end
+
+
+puts "PART 2"
+
+def with_debugging
+	puts "Got Here!"
+
+	result =  yield
+
+	puts "Result was #{result}"
+end
+
+with_debugging do
+  magic_number = (23 - Time.now.hour) * Math::PI
+end
