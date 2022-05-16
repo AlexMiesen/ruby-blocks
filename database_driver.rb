87d168513d47e56d178da7537008a5fe914dade1
AlexMiesen <alex@i-motor.com.au>
2022-05-16 23:06:58 +1000

    Added transactionally method for bonus round

--- database_driver.rb
+++ database_driver.rb
@@ -24,6 +24,27 @@ class DatabaseDriver
     # executes SQL
   end
 
+  def transactionally
+    begin_transaction
+    yield
+    commit_transaction
+  rescue Exception => e
+    rollback_transaction
+  end
+  
+  def begin_transaction
+    puts "Beginning transaction..."
+  end
+  
+  def commit_transaction
+    puts "Committed transaction."
+  end
+  
+  def rollback_transaction
+    puts "Rolled back transaction!"
+  end
+  
+
 	def self.open(database, admin, password)
 		driver = DatabaseDriver.new(database, admin, password)
 		driver.connect
@@ -40,24 +61,35 @@ class DatabaseDriver
 
 end
 
-driver = DatabaseDriver.new("my_database", "admin", "secret")
+# driver = DatabaseDriver.new("my_database", "admin", "secret")
 
-driver.connect
-driver.execute("SELECT * FROM ORDERS")
-driver.execute("SELECT * FROM USERS")
-driver.disconnect
+# driver.connect
+# driver.execute("SELECT * FROM ORDERS")
+# driver.execute("SELECT * FROM USERS")
+# driver.disconnect
 
-puts "New code below"
+# puts "New code below"
+
+# DatabaseDriver.open("my_database", "admin", "secret") do |driver|
+#   driver.execute("SELECT * FROM ORDERS")
+#   driver.execute("SELECT * FROM USERS")
+# end
+
+# puts "raised error below"
+
+# DatabaseDriver.open("my_database", "admin", "secret") do |driver|
+#   driver.execute("DELETE * FROM ORDERS")
+#   raise "Boom!"
+#   driver.execute("DELETE * FROM USERS")
+# end
+
+
+puts "Bonus Part:"
 
 DatabaseDriver.open("my_database", "admin", "secret") do |driver|
-  driver.execute("SELECT * FROM ORDERS")
-  driver.execute("SELECT * FROM USERS")
-end
-
-puts "raised error below"
-
-DatabaseDriver.open("my_database", "admin", "secret") do |driver|
-  driver.execute("DELETE * FROM ORDERS")
-  raise "Boom!"
-  driver.execute("DELETE * FROM USERS")
+  driver.transactionally do
+    driver.execute("UPDATE ORDERS SET status='completed'")
+    raise "Boom!"
+    driver.execute("DELETE * FROM SHIPPING_QUEUE")
+  end
 end
